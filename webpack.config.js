c01a46005bf508a7f8f7a9da937c19c3b219d227
evgalp <evgeniyalpayev@gmail.com>
2018-03-10 13:44:17 +0200

    basic config: js and css/scss loaders, babel, image loaders

---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+module.exports = {
+  entry: "./src/js/app.js",
+  output: {
+    path: __dirname + "/public",
+    filename: "bundle.js"
+  },
+  devServer: {
+    contentBase: __dirname + "/public"
+  },
+  module: {
+    rules: [
+      {
+        loader: "babel-loader",
+        test: /\.js/
+      },
+      {
+        test: /\.s?css/,
+        use: [
+          "style-loader",
+          "css-loader",
+          "sass-loader"
+        ]
+      },
+      {
+        test: /\.(gif|png|jpe?g|svg)/i,
+        use: [
+          "file-loader",
+          {
+            loader: "image-webpack-loader",
+            options: {
+              gifsicle: {
+                interlanced: false
+              },
+              optipng: {
+                optimizationLevel: 7
+              },
+              pngquant: {
+                quality: "65-90",
+                speed: 4
+              },
+              mozjpeg: {
+                progressive: true,
+                quality: 65
+              }
+            }
+          }
+        ]
+      }
+    ]
+  }
+}
